277a16aef9a9a257b77e1dad60a9eedec58d15d1
admin <ruud.lameijer@dataiku.com>
2021-08-27 15:34:30 +0200

    Saved recipe: DKU_TUTORIAL_BASICS_101.compute_car_data

---
+++ recipes/compute_car_data.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Generate some data
+cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
+        'Price': [22000,25000,27000,35000]
+        }
+
+# Convert to dataframe
+car_data_df = pd.DataFrame(cars, columns = ['Brand', 'Price'])
+
+# Write recipe outputs
+car_data = dataiku.Dataset("car_data")
+car_data.write_with_schema(car_data_df)
